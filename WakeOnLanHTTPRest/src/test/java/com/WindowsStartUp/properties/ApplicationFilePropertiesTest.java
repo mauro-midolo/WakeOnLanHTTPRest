fa7580a71a18e7cf48d4e3fe842c9d5be6bc2580
Mauro Midolo <mauro.midolo@hotmail.com>
2018-10-07 18:24:21 +0200

    added unit tests

--- WakeOnLanHTTPRest/src/test/java/com/WindowsStartUp/properties/ApplicationFilePropertiesTest.java
+++ WakeOnLanHTTPRest/src/test/java/com/WindowsStartUp/properties/ApplicationFilePropertiesTest.java
@@ -1,8 +1,18 @@
 package com.WindowsStartUp.properties;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.Objects;
 
 public class ApplicationFilePropertiesTest {
 
 
+    @Test
+    public void shouldCreateInstance() {
+        new ApplicationFileProperties(getPathOf("test1.properties"));
+    }
+
+    private String getPathOf(String name) {
+        return Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(name)).getPath();
+    }
 }
