e4394d862308ba7c110ff5989e5672c0a06ebcae
Mauro Midolo <mauro.midolo@hotmail.com>
2018-10-17 22:38:58 +0200

    Added unit tests

--- WakeOnLanHTTPRest/src/test/java/com/WindowsStartUp/properties/ApplicationFilePropertiesTest.java
+++ WakeOnLanHTTPRest/src/test/java/com/WindowsStartUp/properties/ApplicationFilePropertiesTest.java
@@ -4,12 +4,61 @@ import org.junit.Test;
 
 import java.util.Objects;
 
-public class ApplicationFilePropertiesTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
+public class ApplicationFilePropertiesTest {
 
     @Test
     public void shouldCreateInstance() {
         new ApplicationFileProperties(getPathOf("test1.properties"));
+    }
+
+    @Test
+    public void shouldReturnsAValueWithoutDefault() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        String result = applicationProperties.getProperty("mac.address");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void shouldReturnsAValueWithDefault() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        String result = applicationProperties.getProperty("mac.address", "DefaultValue");
+        assertNotEquals("DefaultValue", result);
+    }
+
+    @Test
+    public void shouldReturnsDefaultValueIfVariableNotSet() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        String result = applicationProperties.getProperty("notExists", "DefaultValue");
+        assertEquals("DefaultValue", result);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldRaiseAnErrorIfFileNotExists() {
+        new ApplicationFileProperties("notExistsFile.properties");
+    }
+
+    @Test
+    public void shouldReturnsIntValue() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        int result = applicationProperties.getIntProperty("http.port", 91);
+        assertEquals(8080, result);
+    }
+
+    @Test
+    public void shouldReturnsDefaultValueIfNotSet() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        int result = applicationProperties.getIntProperty("notSet", 91);
+        assertEquals(91, result);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldRaiseAnErrorIfVariableNotANumber() {
+        ApplicationProperties applicationProperties = new ApplicationFileProperties(getPathOf("test1.properties"));
+        applicationProperties.getIntProperty("mac.address", 91);
     }
 
     private String getPathOf(String name) {
