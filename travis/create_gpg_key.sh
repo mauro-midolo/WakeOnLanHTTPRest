9ad793992a24a47cefe133c104e226b4f474ab9b
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-04 16:47:17 +0200

    create gpg

---
+++ travis/create_gpg_key.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+Passphrase=${env.PASSPHRASE}
+NameReal=${env.MYNAME}
+NameEmail=${env.MYEMAIL}
+cat > keydetails <<EOF
+    %echo Generating a basic OpenPGP key
+    Key-Type: RSA
+    Key-Length: 2048
+    Subkey-Type: RSA
+    Subkey-Length: 2048
+    Name-Real: ${NameReal}
+    Name-Email: ${NameEmail}
+    Passphrase: ${Passphrase}
+    Expire-Date: 365
+    %commit
+    %echo done
+EOF
+
+gpg --verbose --batch --gen-key keydetails
+
+gpg --keyserver keyserver.ubuntu.com --send-keys
+
