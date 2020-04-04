893bdd8582b68749b035af49ea317ab7945d2a89
Mauro <mauro@AsusVX6.lan>
2020-04-04 23:24:49 +0200

    fixed -f

--- .travis/maven_release.sh
+++ .travis/maven_release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mvn release:prepare -B -DdryRun=true
+mvn release:prepare -B -DdryRun=true -f WakeOnLanHTTPRest
 mvn clean release:prepare --settings .travis/settings.xml -f WakeOnLanHTTPRest -DskipTests
 mvn release:perform --settings .travis/settings.xml -f WakeOnLanHTTPRest -DskipTests
 git push
