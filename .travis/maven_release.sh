0bb5e81fa1b56c5af994130e31f431338d60afb3
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-05 18:40:11 +0200

    specify version

--- .travis/maven_release.sh
+++ .travis/maven_release.sh
@@ -5,7 +5,7 @@ set -e
 git remote set-url origin git@github.com:mauro-midolo/WakeOnLanHTTPRest.git
 git checkout master
 git pull
-mvn initialize
+mvn initialize -f WakeOnLanHTTPRest
 cat release.properties
 echo "HEREEE"
 mvn release:clean release:prepare release:perform -B --settings .travis/settings.xml -f WakeOnLanHTTPRest
