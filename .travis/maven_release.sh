f8e22119c35c0a14c91183870af2499dc01229b5
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-05 16:37:22 +0200

    changed url

--- .travis/maven_release.sh
+++ .travis/maven_release.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git remote set-url git@github.com:mauro-midolo/WakeOnLanHTTPRest.git
 git checkout master
+git pull
 mvn initialize release:clean release:prepare release:perform -B --settings .travis/settings.xml -f WakeOnLanHTTPRest
 git push
