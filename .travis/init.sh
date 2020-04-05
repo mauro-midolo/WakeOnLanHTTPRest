0d801191ce6411405dce95ae0b5ea7be3d0975a6
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-05 18:23:13 +0200

    mykey

--- .travis/init.sh
+++ .travis/init.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
-Passphrase=${PASSPHRASE}
-NameReal=${MYNAME}
-NameEmail=${MYEMAIL}
 MyAccess=${MYACCESS}
-cat > keydetails <<EOF
-    %echo Generating a basic OpenPGP key
-    Key-Type: RSA
-    Key-Length: 2048
-    Subkey-Type: RSA
-    Subkey-Length: 2048
-    Name-Real: ${NameReal}
-    Name-Email: ${NameEmail}
-    Passphrase: ${Passphrase}
-    Expire-Date: 365
-    %commit
-    %echo done
-EOF
+MyKey=${MYKEY}
 mkdir ~/.ssh
 echo "${MyAccess}" | base64 --decode > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
-gpg --verbose --batch --gen-key keydetails > /dev/null 2>&1
-#gpg --keyserver keyserver.ubuntu.com --send-keys
+echo "${MyKey}" | base64 --decode > mykey.key
+chmod 400 mykey.key
+gpg --import mykey.key
 
