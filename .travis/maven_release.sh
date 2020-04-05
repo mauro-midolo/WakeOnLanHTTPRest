ffd07cf6883294ce2964f60d56c470f7312af018
Mauro <mauro@AsusVX6.lan>
2020-04-05 12:57:22 +0200

    removed first release

--- .travis/maven_release.sh
+++ .travis/maven_release.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-mvn release:prepare -B -DdryRun=true -f WakeOnLanHTTPRest
 mvn clean release:prepare --settings .travis/settings.xml -f WakeOnLanHTTPRest -DskipTests
 mvn release:perform --settings .travis/settings.xml -f WakeOnLanHTTPRest -DskipTests
 git push
