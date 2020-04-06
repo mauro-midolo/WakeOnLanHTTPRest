688ad6eaba12fc7715bfd2a90bfb3e49b4a80c6a
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-06 18:28:02 +0200

    Added tests

---
+++ WakeOnLanHTTPRest/src/test/java/com/mauromidolo/windowsstartup/NetworkRepositoryImplTest.java
@@ -0,0 +1,28 @@
+package com.mauromidolo.windowsstartup;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class NetworkRepositoryImplTest {
+
+    @Test
+    public void shouldExist() {
+        NetworkRepositoryImpl networkRepository = new NetworkRepositoryImpl();
+        Assert.assertNotNull(networkRepository);
+    }
+
+    @Test
+    public void shouldReturnIps() {
+        NetworkRepositoryImpl networkRepository = new NetworkRepositoryImpl();
+        List<String> broadCastIps = networkRepository.getBroadCastIps();
+        Assert.assertFalse(broadCastIps.isEmpty());
+    }
+
+    @Test
+    public void shouldExecuteWakeOnLan() {
+        NetworkRepositoryImpl networkRepository = new NetworkRepositoryImpl();
+        networkRepository.executeWOL("00-00-00-00-00-00","192.168.0.255");
+    }
+}
