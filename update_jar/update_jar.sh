7548acb7bca64c61fb7f02128cedee8961e9df06
Mauro Midolo <mauro.midolo@hotmail.com>
2020-04-27 22:04:41 +0200

    Added updated script

---
+++ update_jar/update_jar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JAR_ABSOLUTE_PATH=/opt/wol/WakeOnLanHTTPRest.jar
+
+echo "Cleaning space..."
+sudo rm -f "$JAR_ABSOLUTE_PATH"
+echo "Downloading latest verion..."
+runuser -l wol -c "curl -Ls -o \"$JAR_ABSOLUTE_PATH\" \"https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.github.tousinho&a=Tinei-Matewai&v=LATEST&e=jar&c=jar-with-dependencies\""
+
+echo "Restaring Service..."
+sudo service wol restart
