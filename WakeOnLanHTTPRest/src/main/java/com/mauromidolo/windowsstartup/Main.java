ccab5e047dc8c36941830595caa9a374e30078e6
Midolo Mauro <mauro.midolo@csebo.it>
2023-02-16 11:49:57 +0100

    Changed jetty-server with Spring

--- WakeOnLanHTTPRest/src/main/java/com/mauromidolo/windowsstartup/Main.java
+++ WakeOnLanHTTPRest/src/main/java/com/mauromidolo/windowsstartup/Main.java
@@ -1,19 +1,12 @@
 package com.mauromidolo.windowsstartup;
 
-import com.mauromidolo.windowsstartup.jetty.JettyHttpServer;
-import com.mauromidolo.windowsstartup.jetty.WebServer;
 import com.mauromidolo.windowsstartup.properties.ApplicationFileProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
-        ApplicationFileProperties applicationFileProperties = new ApplicationFileProperties("./server.properties");
-        int httpPort = applicationFileProperties.getIntProperty("http.port", 8080);
-        WebServer httpServer = new JettyHttpServer(httpPort);
-        try {
-            httpServer.start();
-            httpServer.join();
-        } finally {
-            httpServer.destroy();
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
